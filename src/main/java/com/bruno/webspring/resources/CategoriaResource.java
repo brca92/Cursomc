ce975f018c9047fc2422aadcb2097db4ec0e6ee2
Bruno <bruno_ca92@hotmail.com>
2019-01-02 18:40:45 -0200

    Primeira classe de dominio criada

--- src/main/java/com/bruno/webspring/resources/CategoriaResource.java
+++ src/main/java/com/bruno/webspring/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.bruno.webspring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bruno.webspring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,8 +15,16 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cati1=new Categoria(1,"InformÃ¡tica");
+		Categoria cati2=new Categoria(2,"EscritÃ³rio");
+		
+		List<Categoria> lista=new ArrayList<>();
+		lista.add(cati1);
+		lista.add(cati2);
+		
+		return lista;
 	}
 	
 }
