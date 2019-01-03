555c46ab321af892d6fea45c8175cd3bf9b3559e
Bruno <bruno_ca92@hotmail.com>
2019-01-03 11:38:41 -0200

    Criando operacao de instanciacao

--- src/main/java/com/bruno/webspring/WebSpringApplication.java
+++ src/main/java/com/bruno/webspring/WebSpringApplication.java
@@ -1,13 +1,34 @@
 package com.bruno.webspring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bruno.webspring.domain.Categoria;
+import com.bruno.webspring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class WebSpringApplication {
+public class WebSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 =new Categoria(null, "Ïnformatica");
+		Categoria cat2 =new Categoria(null, "Escritorio");
+	
+		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
+	
 	}
 
 }
